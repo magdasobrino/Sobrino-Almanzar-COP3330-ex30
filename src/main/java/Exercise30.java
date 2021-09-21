4116e6ab5ea7ac0a81b241258ab336c5901f85d6
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-20 22:42:13 -0400

    Initial commit

---
+++ src/main/java/Exercise30.java
@@ -0,0 +1,13 @@
+
+
+public class Exercise30 {
+
+public static void main(String[] args) {
+
+        MultiplicationTable generate = new MultiplicationTable();
+        String output = generate.generateTables();
+
+        System.out.print(output);
+
+        }
+        }
